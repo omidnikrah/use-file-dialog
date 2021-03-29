5091c307bff4c91eb1aa0c5761b2db59c3ad6d4e
Soroush Chehresa <s1996ch@gmail.com>
2021-03-29 11:45:17 +0430

    Improve types

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 
-const useFileDialog = (onSelectFile?: (files: any) => void) => {
-	const [files, setFiles] = useState(null);
+const useFileDialog = (onSelectFile?: (files: FileList) => void) => {
+	const [files, setFiles] = useState<FileList | null>(null);
 
 	const openFileDialog = () => {
 		const input = document.createElement('input');
 		input.type = 'file';
 
 		input.onchange = (event: Event) => {
-			const { files } = event.target as HTMLInputElement;
+			const { files }: {files: FileList} = event.target as HTMLInputElement;
 			setFiles(files);
 			if (onSelectFile) {
 				onSelectFile(files);
